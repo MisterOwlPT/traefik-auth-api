8f5f8fe3b0a4667fa88f521e8e9b56b8e4419e9a
Pedro Miguel Melo <pedro.m.melo@inesctec.pt>
2023-03-03 09:53:38 +0000

    Removed dependency 'cors' from backend service.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,5 @@
-const cors = require('cors')
 const express = require('express');
 const app = express();
-
-// Without CORS an error would be thrown everytime a request is made to the API!
-// TODO: set headers as required per use case!
-app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello from backend server!');
